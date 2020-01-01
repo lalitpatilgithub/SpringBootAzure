e392e92b1972e1a745004c36c0001e1040352965
Lalit <lalitpatil481@gmail.com>
2020-01-01 16:12:30 +0530

    First commit

---
+++ src/main/java/com/springbootexample/SpringBootStartupClass.java
@@ -0,0 +1,14 @@
+package com.springbootexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class SpringBootStartupClass {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStartupClass.class);
+    }
+
+}
